932b2780623484460b3fc2d377ac7ceb25c4a162
nublius <184036331+nublius@users.noreply.github.com>
2025-04-08 13:09:50 -0700

    simple budgeting function

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function getBudget(income) {
+    const needs = income / 2;
+    console.log(needs);
+    const wants = income * (30/100);
+    console.log(wants);
+    const savings = income * (20/100);
+    console.log(savings);
+}
